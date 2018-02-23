807da85902ef1b39d25fa8f31369f63add04a455
yemax <egormaksimchuk@gmail.com>
2018-02-23 16:19:48 +0200

    refactoring

---
+++ servlets-xml/src/main/java/com/company/servlets/ExampleFilterServlet.java
@@ -0,0 +1,33 @@
+package com.company.servlets;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Date;
+
+public class ExampleFilterServlet implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter created!");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String ipAddress = request.getRemoteAddr();
+        String dateTime = new Date().toString();
+
+        System.out.println("\n\n==============================================\n");
+
+        System.out.println("Request...");
+        System.out.println("Date/Time: " + dateTime);
+        System.out.println("IP:" + ipAddress);
+
+        System.out.println("\n==============================================\n");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
