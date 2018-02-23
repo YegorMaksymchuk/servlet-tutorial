caee53e0ecc953978c9de99964af2fb813a70135
yemax <egormaksimchuk@gmail.com>
2018-02-23 23:14:02 +0200

    refactoring

---
+++ servlets-annotations/src/main/java/com/company/servlets/MyServlet.java
@@ -0,0 +1,21 @@
+package com.company.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("<h1>Yeeeeeee!!!</h1>");
+
+        req.getRequestDispatcher("/jsp/test.jsp").forward(req, resp);
+
+    }
+}
